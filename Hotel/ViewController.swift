bf7ff4561e2d9db33b6a3325d3f0ad4abb266fe5
Usov Nikita <usov107@gmail.com>
2023-09-05 15:30:36 +0700

    Initial Commit

---
+++ Hotel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hotel
+//
+//  Created by Nikita Usov on 05.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
